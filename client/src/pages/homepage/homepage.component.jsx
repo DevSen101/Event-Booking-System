9c84642664e7947544c67f3515f45999078f9039
Dev Sen <devksen.personal@gmail.com>
2025-12-12 17:28:23 +0530

    initial commit: create frontend structure (components, pages, assets)

---
+++ client/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.styles.css"
+
+const Homepage = () => (
+ <div>
+  <h1>Welcome to Event Management System</h1>
+ </div>
+)
+
+export default Homepage;
